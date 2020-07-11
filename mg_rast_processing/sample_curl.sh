85b3a69b56809666bc88e0f6035db207debede4e
Ryan Bartelme <r.p.bartelme@gmail.com>
2020-07-10 17:36:09 -0700

    Success! Working bash curl while loop for MG-RAST RESTful API.

--- mg_rast_processing/sample_curl.sh
+++ mg_rast_processing/sample_curl.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+# this takes tsv output from mg_json_parse.R
 # use read in shell built-in to parse the tsv
-while IFS=" " read -r value1 value2 remainder
+while IFS=$'\t' read -r value1 value2 remainder
 do
     curl -X GET -H \
     "auth: your_mg_rast_API_key" \
-    $value1 \
-    > ${value2}.json
-done < "test_input.txt" > "test_out.txt"
+    "${value2}" \
+    >> ${value1}.json
+done < "test_input.txt"
