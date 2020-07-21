eed3615d7275d1fc10253e500465b2020a2dae2b
Ryan Bartelme <r.p.bartelme@gmail.com>
2020-07-21 14:42:36 -0700

    minor edits

--- mg_rast_processing/neon_sample_tabulate.R
+++ mg_rast_processing/neon_sample_tabulate.R
@@ -2,6 +2,7 @@
 
 library(jsonlite)
 library(tidyverse)
+library(googlesheets4)
 
 # read in all json files in the directory as a list
 
@@ -26,5 +27,38 @@ names(meta_df) <- gsub("1", "", names(meta_df))
 # gsub: all column names ending in 2, repalce with _url
 names(meta_df) <- gsub("2", "_url", names(meta_df))
 
+# -----------------------------------------------------------
+
+# import from google sheets directly with my API authentication
+neon_sites <- read_sheet("https://docs.google.com/spreadsheets/d/1RFBirlPtoZJzdIoYTTOemO2olg3u85VQyCfu-Hspdas/edit?usp=sharing")
+
+# site codes for grep or group_by
+targets <- neon_sites$`Sites with 3+ Metagenome Sequences`
+
+# -----------------------------------------------------------
+# counts by site from the metagenome JSON API
+# -----------------------------------------------------------
+
+# extract side codes to match gsheets 
+json_sites <- gsub( "_.*", "", meta_df$name)
+
+# make a dataframe of counts for each site
+json_counts <- as.data.frame(as.matrix(table(json_sites)))
+
+# make row names into a column
+json_counts <- rownames_to_column(json_counts, "sites")
+
+# rename second colname
+
+names(json_counts)[2] <- "json_api_counts"
 
 
+names(neon_sites)[1] <- "sites"
+
+names(neon_sites)[2] <- "neon_api_counts" 
+
+count_comparison <- left_join(neon_sites[, 1:2], json_counts, by = "sites")
+
+neonurl <- "https://docs.google.com/spreadsheets/d/13iqnppyx7m8yoZAxRmr0wDpNyDq_NQglXTaTmXMDFRc/edit?usp=sharing"
+
+sheet_write(data = count_comparison, ss = neonurl, sheet = "Sheet1")
